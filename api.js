9118109b955cd165d033dff5433c85de72be62fb
Kyle Steffel <cheesykyle@users.noreply.github.com>
2021-12-07 23:04:07 -0600

    Update api.js

--- api.js
+++ api.js
@@ -73,7 +73,7 @@ module.exports = function (req, res, next) {
                         responseObj['rows'] = rows;
                     }
                     if (showOwed === true) {
-                        amountOwed = responseObj['rows']['Owed To HCS'];
+                        amountOwed = responseObj['rows'][0]{4};
                     }
                     return res.status(200).json(amountOwed);
                 } else {     
